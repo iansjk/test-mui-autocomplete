c4cec7db4157edb60c0d34c529334d36359c6d9e
Ian Kim <iansjk@gmail.com>
2020-09-22 01:11:24 -0400

    Add code to reproduce Autocomplete rendering bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Autocomplete from '@material-ui/lab/Autocomplete/Autocomplete';
+import { TextField } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Autocomplete
+              options={['a', 'b', 'c']}
+              renderInput={(params) => (
+                <TextField
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...params}
+                  label="Autocomplete 1"
+                  variant="outlined"
+                />
+              )}
+        />
+        <Autocomplete
+                  options={['d', 'e', 'f']}
+                  renderInput={(params) => (
+                    // eslint-disable-next-line react/jsx-props-no-spreading
+                    <TextField {...params} label="Autocomplete 2" variant="outlined" />
+                  )}
+                />
       </header>
     </div>
   );
